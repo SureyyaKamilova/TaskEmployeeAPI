74cc23e794125128ecc22c22b4bef13a5c1405e2
user <user@DESKTOP-DDOO6UB>
2024-10-24 11:55:12 +0400

    Add Entities

---
+++ Entities/DTO/EmployeeDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class EmployeeDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string DepartmentName { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
