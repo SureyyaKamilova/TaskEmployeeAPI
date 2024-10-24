970d29f8d53cfad7436b315824c8e26b44582798
user <user@DESKTOP-DDOO6UB>
2024-10-24 13:00:04 +0400

    Add Dependency Injection services and repositories

--- TaskEmployeeAPI/Program.cs
+++ TaskEmployeeAPI/Program.cs
@@ -1,3 +1,8 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IEmployeeDal, EmployeeDal>();
+builder.Services.AddSingleton<IEmployeeService, EmployeeManager>();
+
+builder.Services.AddSingleton<IDepartmentDal, DepartmentDal>();
+builder.Services.AddSingleton<IDepartmentService, DepartmentManager>();
+
+builder.Services.AddSingleton<ICompanyDal, CompanyDal>();
+builder.Services.AddSingleton<ICompanyService, CompanyManager>();
 
 var app = builder.Build();
 
